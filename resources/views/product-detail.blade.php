9d02ff7a6c689acf7346120b55e58ea4b0d61d3b
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2015-06-13 15:08:00 -0500

    Se removio el FB plugin para version tablet y movil, reducido el padding de product-detail y en  la descripcion

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -2,94 +2,93 @@
 
 @section('content')
 <div class="row">
-	<div class="col-lg-3 col-md-3 col-sm-12">
+	<div class="col-md-3 hidden-sm hidden-xs">
 		@include('partials.fb-iframe')
 	</div>
-
-	<div class="clearfix visible-sm"></div>
-
 	<div class="col-lg-9 col-md-9 col-sm-12">
-		<div class="col-lg-12 col-sm-12">
-    		<span class="title">{{ $product->name }}</span>
-    	</div>
-        <div class="col-lg-12 col-sm-12 hero-feature">
-			<div class="row">
-				<div class="col-lg-6 col-md-6 col-sm-6">
-					<!-- Main Image -->
-					<div class="product-main-image-container">
-						<img src="{{ asset('img/loader.gif') }}" alt="" class="product-loader">
-						<span class="thumbnail product-main-image">
-							<img src="{{ asset('img/products/' . $product->default_img) }}" alt="{{ $product->default_img }}">
-						</span>
+		<div class="row">
+			<div class="col-sm-12">
+	    		<span class="title">{{ $product->name }}</span>
+	    	</div>
+	        <div class="col-sm-12 hero-feature">
+				<div class="row">
+					<div class="col-sm-6">
+						<!-- Main Image -->
+						<div class="product-main-image-container">
+							<img src="{{ asset('img/loader.gif') }}" alt="" class="product-loader">
+							<span class="thumbnail product-main-image">
+								<img src="{{ asset('img/products/' . $product->default_img) }}" alt="{{ $product->default_img }}">
+							</span>
+						</div>
+						<!-- End Main Image -->
+						<!-- Thumbnail Image -->
+						@foreach($images as $image)
+						<div class="col-xs-3 product-thumb-image">
+							<?php $url_img = asset('img/products/' . $image->img); ?>
+							<a href="{{ $url_img }}" class="thumbnail">
+								<img src="{{ $url_img }}" alt="{{ $image->img }}">
+							</a>
+						</div>
+						@endforeach
+						<!-- End Thumbnail Image -->
 					</div>
-					<!-- End Main Image -->
-					<!-- Thumbnail Image -->
-					@foreach($images as $image)
-					<div class="col-xs-3 product-thumb-image">
-						<?php $url_img = asset('img/products/' . $image->img); ?>
-						<a href="{{ $url_img }}" class="thumbnail">
-							<img src="{{ $url_img }}" alt="{{ $image->img }}">
-						</a>
-					</div>
-					@endforeach
-					<!-- End Thumbnail Image -->
-				</div>
 
-				<div class="visible-xs">
+					<div class="visible-xs">
+						<div class="clearfix"></div>
+					</div>
+
+					<div class="col-sm-6">
+						<div class="well well-sm product-short-detail">
+							<div class="row">
+								<div class="the-list">
+									<h3 class="col-xs-12">
+										<!-- <span class="price-old">$169</span> -->
+										{{ $product->currency == 'S' ? 'S/. ' : '$. '}} {{ $product->price }}
+									</h3>
+								</div>
+								<div class="the-list">
+									<div class="col-xs-4">
+										<span>Disponible</span>
+									</div>
+									<div class="col-xs-8">
+										<!-- <span class="red">OUT OF STOCK</span> -->
+										<span class="green">En Stock</span>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+								<hr/>
+								{!! Form::open(['route' => 'registerClientData']) !!}
+								<input type="hidden" name="product_id" value="{{ $product->id }}">
+								<div class="col-xs-12 input-qty-detail">
+									<input type="text" name="quanty" class="form-control input-qty text-center" value="1">
+									<button class="btn btn-default pull-left"><i class="fa fa-shopping-cart"></i> Quiero</button>
+								</div>
+								{!! Form::close() !!}
+							</div>
+						</div>
+					</div>
+
 					<div class="clearfix"></div>
-				</div>
+					<br clear="all"/>
 
-				<div class="col-lg-6 col-md-6 col-sm-6">
-					<div class="well product-short-detail">
-						<div class="row">
-							<div class="the-list">
-								<h3 class="col-xs-12">
-									<!-- <span class="price-old">$169</span> -->
-									{{ $product->currency == 'S' ? 'S/. ' : '$. '}} {{ $product->price }}
-								</h3>
-							</div>
-							<div class="the-list">
-								<div class="col-xs-4">
-									<span>Disponible</span>
+					<div class="col-xs-12 product-detail-tab">
+						<!-- Nav tabs -->
+						<ul class="nav nav-tabs">
+							<li class="active"><a href="#desc" data-toggle="tab">Description</a></li>
+						</ul>
+
+						<!-- Tab panes -->
+						<div class="tab-content">
+							<div class="tab-pane active" id="desc">
+								<div class="well">
+									{!! $product->description !!}
 								</div>
-								<div class="col-xs-8">
-									<!-- <span class="red">OUT OF STOCK</span> -->
-									<span class="green">En Stock</span>
-								</div>
-							</div>
-							<div class="clearfix"></div>
-							<hr/>
-							{!! Form::open(['route' => 'registerClientData']) !!}
-							<input type="hidden" name="product_id" value="{{ $product->id }}">
-							<div class="col-xs-12 input-qty-detail">
-								<input type="text" name="quanty" class="form-control input-qty text-center" value="1">
-								<button class="btn btn-default pull-left"><i class="fa fa-shopping-cart"></i> Quiero</button>
-							</div>
-							{!! Form::close() !!}
-						</div>
-					</div>
-				</div>
-
-				<div class="clearfix"></div>
-				<br clear="all"/>
-
-				<div class="col-xs-12 product-detail-tab">
-					<!-- Nav tabs -->
-					<ul class="nav nav-tabs">
-						<li class="active"><a href="#desc" data-toggle="tab">Description</a></li>
-					</ul>
-
-					<!-- Tab panes -->
-					<div class="tab-content">
-						<div class="tab-pane active" id="desc">
-							<div class="well">
-								{!! $product->description !!}
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-        </div>
+	        </div>
+		</div>
 	</div>
 </div>
 @stop
